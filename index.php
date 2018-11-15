d08552a49e92493082c885ba0c578c34e1e53bc7
muralikrishna2basa <muralikrishna2basa@gmail.com>
2018-11-15 19:56:59 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+   <head>
+      <title>Online PHP-7 Script Execution</title>      
+   </head>
+   
+   <body>
+      
+      <?php
+         echo "<h1>Hello, PHP-7!</h1>";
+      ?>
+   
+   </body>
+</html>
